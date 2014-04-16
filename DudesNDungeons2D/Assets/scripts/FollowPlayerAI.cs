4fce5cc5702f974fa5703c4fc3a34968a5851f61
Lokir <Phauga12@student.aau.dk>
2014-04-16 10:52:17 +0200

    Corrections, Tobias look at this.

--- DudesNDungeons2D/Assets/scripts/FollowPlayerAI.cs
+++ DudesNDungeons2D/Assets/scripts/FollowPlayerAI.cs
@@ -1,39 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FollowPlayerAI : MonoBehaviour {
-
+public class FollowPlayerAI : MonoBehaviour 
+{
 	//Variables for the enemy detecting the player
-	Transform playerFound;
-	Transform enemyLocation;
-	float aiSpeed = 0.05f;
+	Transform fPlayer;
+	Vector3 ePosition;
+	float eDistance;
+	float eSpeed = 0.05f;
 	
 	// Use this for initialization
-	void Start () {
-		playerFound = GameObject.FindGameObjectWithTag("Player").transform;
-		enemyLocation = GameObject.FindGameObjectWithTag ("EnemyLocation").transform;
+	void Start () 
+	{
+		fPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+		ePosition = transform.position;
 	}
-	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		eDistance = Vector3.Distance(transform.position, fPlayer.position);
 		enemyAI();
 	}
-
 	void enemyAI(){
-		if(Vector3.Distance(transform.position, playerFound.position) <= 5.0f && Vector3.Distance(transform.position, playerFound.position) >= 1.5f){
+		if(eDistance <= 10 && eDistance >= 1.5f)
+		{
 			enemyAIFollow();
 		}
-
-		if(Vector3.Distance(transform.position, playerFound.position) == 1.5f){
+		else if(eDistance <= 1.5f)
+		{
 			enemyAIAttack();
 		}
+		else // return to origin.
+		{
+			if(transform.position != ePosition)
+			returnToStart ();
+		}
 	}
+	void enemyAIFollow()
+	{
+		if(fPlayer.position.x < transform.position.x)
+			transform.position += new Vector3(-eSpeed, 0, 0);
 
-	void enemyAIFollow(){
-		transform.position += new Vector3(-aiSpeed, 0, 0);
+		else if(fPlayer.position.x > transform.position.x)
+			transform.position += new Vector3(eSpeed, 0, 0);
 	}
+	void enemyAIAttack()
+	{
+		Debug.Log ("ATTACK!");
+	}
+	void returnToStart()
+	{
+		if(transform.position.x < ePosition.x)
+			transform.position += new Vector3(eSpeed, 0, 0);
 
-	void enemyAIAttack(){
-		
+		else if(transform.position.x > ePosition.x)
+			transform.position += new Vector3(-eSpeed, 0, 0);
 	}
 }
