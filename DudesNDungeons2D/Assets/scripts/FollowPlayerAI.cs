d91b8daffef6c6394da951bf748227e6f5c0c3bc
toszi <toszi18@gmail.com>
2014-04-16 11:06:33 +0200

    Commented AI system, still missing fighting function

--- DudesNDungeons2D/Assets/scripts/FollowPlayerAI.cs
+++ DudesNDungeons2D/Assets/scripts/FollowPlayerAI.cs
@@ -12,25 +12,27 @@ public class FollowPlayerAI : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		fPlayer = GameObject.FindGameObjectWithTag("Player").transform;
-		ePosition = transform.position;
+		fPlayer = GameObject.FindGameObjectWithTag("Player").transform; //locates an object with the tag Player
+		ePosition = transform.position; //saves the position of the object this script is attached to
 	}
 	// Update is called once per frame
 	void Update () 
 	{
-		eDistance = Vector3.Distance(transform.position, fPlayer.position);
+		eDistance = Vector3.Distance(transform.position, fPlayer.position); //This is the distance between the player and the enemy, it currently
+																			//isn't looking for any specific distance
 		enemyAI();
 	}
 	void enemyAI(){
-		if(eDistance <= 10 && eDistance >= 1.5f)
+		if(eDistance <= 10 && eDistance >= 1.5f) //here we use the eDistance to make the enemy follow the player if he is within a certain range
+												 //and stop if he comes to close to the player
 		{
 			enemyAIFollow();
 		}
-		else if(eDistance <= 1.5f)
+		else if(eDistance <= 1.5f) //If the enemy is close to the player he will call the attack function
 		{
 			enemyAIAttack();
 		}
-		else // return to origin.
+		else // return to origin
 		{
 			if(transform.position != ePosition)
 			returnToStart ();
@@ -38,8 +40,8 @@ public class FollowPlayerAI : MonoBehaviour
 	}
 	void enemyAIFollow()
 	{
-		if(fPlayer.position.x < transform.position.x)
-			transform.position += new Vector3(-eSpeed, 0, 0);
+		if(fPlayer.position.x < transform.position.x) //these two if-statements are to see if the player is either on one site or the other
+			transform.position += new Vector3(-eSpeed, 0, 0);//and the enemy has to move in that direction
 
 		else if(fPlayer.position.x > transform.position.x)
 			transform.position += new Vector3(eSpeed, 0, 0);
@@ -50,7 +52,7 @@ public class FollowPlayerAI : MonoBehaviour
 	}
 	void returnToStart()
 	{
-		if(transform.position.x < ePosition.x)
+		if(transform.position.x < ePosition.x)//if the enemy cannot see the player and is not at his origin he will return to his origin
 			transform.position += new Vector3(eSpeed, 0, 0);
 
 		else if(transform.position.x > ePosition.x)
