b8b230d29e790cfaa314b38cf918ce296986ff7e
Lokir <Phauga12@student.aau.dk>
2014-04-25 16:48:24 +0200

    Sprite size changes, safety update.

--- DudesNDungeons2D/Assets/scripts/AnimHandler.cs
+++ DudesNDungeons2D/Assets/scripts/AnimHandler.cs
@@ -80,8 +80,7 @@ public class AnimHandler : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-
-		for(int i = 0; i<4; i++)
+		for(int i = 0; i<6; i++)
 		{
 			defAtkList.Add(walk1);
 		}
@@ -89,10 +88,15 @@ public class AnimHandler : MonoBehaviour {
 		{
 			defAtkList.Add(defAttack1);
 		}
-		for(int i = 0; i<3; i++)
+		for(int i = 0; i<6; i++)
 		{
 			defAtkList.Add(defAttack2);
 		}
+		for(int i = 0; i<8; i++)
+		{
+			defAtkList.Add(defAttack3);
+		}
+		defAtkList.Add(idle);
 		defAtkList.Add(idle);
 
 		for(int i = 0; i < 6; i++) // Load in 8 of each sprite to correspondent lists
