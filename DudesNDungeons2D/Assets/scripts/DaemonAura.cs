773fe9bbed3362043f2f1af9a9a294d8a18d5743
Lokir <Phauga12@student.aau.dk>
2014-04-26 01:03:30 +0200

    DamageShrine 90% finished.

---
+++ DudesNDungeons2D/Assets/scripts/DaemonAura.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DaemonAura : MonoBehaviour {
+
+	GameObject player;
+	public bool activateAura;
+	
+	public Sprite aura1;
+	public Sprite aura2;
+	public Sprite aura3;
+	public Sprite aura4;
+	public Sprite aura5;
+	public Sprite aura6;
+	public Sprite aura7;
+	public Sprite aura8;
+	public Sprite aura9;
+	public Sprite aura10; // these sprites load the images needed for the animation.
+	public Sprite none; // if not active make sure it is not visible.
+	
+	public List<Sprite> auraList = new List<Sprite>(); // lists to contain the animations,
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player"); // find player.
+		
+		for(int i = 0; i < 4; i++) // load in segments of the animation using all these SMALL for loops, only once.
+			auraList.Add (aura1);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura2);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura3);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura4);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura5);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura6);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura7);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura8);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura9);
+		for(int i = 0; i < 4; i++)
+			auraList.Add (aura10);
+	}
+	// Update is called once per frame
+	int C = 0; // C is used to control the animations.
+	
+	void Update () 
+	{
+		if(activateAura) // if player moving left
+		{
+			transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+			GetComponent<SpriteRenderer>().sprite = auraList[C];
+			if(C >= 19) // make sure list parameters aren't broken.
+				C=0;
+			C++; // advance one step.
+		}
+		
+		if(!activateAura) // if no longer active make render into nothing and set C to 0.
+		{
+			GetComponent<SpriteRenderer>().sprite = none;
+			C = 0;
+		}
+	}
+}
--- DudesNDungeons2D/Assets/scripts/DamShrine.cs
+++ DudesNDungeons2D/Assets/scripts/DamShrine.cs
@@ -5,41 +5,42 @@ public class DamShrine : MonoBehaviour {
 
 	//Damage shrines add 30% to the user for a duration of 20s.
 	//while active the player will take the evil essense of the Daemon as a visual feedback.
-
-	public GameObject DustObject;
-	GameObject playStat;
-	bool dustIsFollowing = true;
+	
+	GameObject DaemonDust;
+	public Sprite Daemon;
+	public Sprite DaemonAura;
+	GameObject player;
 	float buffDuration = 20.0f;
 
 
 
 	// Use this for initialization
 	void Start () {
-		playStat = GameObject.FindGameObjectWithTag("Player");
+		DaemonDust = GameObject.FindGameObjectWithTag("DaemonDust");
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(dustIsFollowing)
-			DustObject.transform.position = playStat.transform.position;
 
-		if(Input.GetKeyDown(KeyCode.E) && !dustIsFollowing)
+		if(Input.GetKeyDown(KeyCode.E) && !DaemonDust.GetComponent<DaemonAura>().activateAura)
 		   buffPlayer();
 	}
 	void buffPlayer()
 	{
 		int tmp;
-		tmp = playStat.GetComponent<player>().pDamage;
-		dustIsFollowing = true;
-		playStat.GetComponent<player>().pDamage += (int)(playStat.GetComponent<player>().pDamage*0.3)+1;
+		tmp = player.GetComponent<player>().pDamage;
+		DaemonDust.GetComponent<DaemonAura>().activateAura = true;
+		GetComponent<SpriteRenderer>().sprite = Daemon;
+		player.GetComponent<player>().pDamage += (int)(player.GetComponent<player>().pDamage*0.3)+1;
 		StartCoroutine("removeBuff", tmp);
 	}
 	IEnumerator removeBuff(int damage)
 	{
 		yield return new WaitForSeconds(buffDuration);
-		playStat.GetComponent<player>().pDamage = damage;
-		dustIsFollowing = false;
-		Destroy(DustObject);
+		player.GetComponent<player>().pDamage = damage;
+		DaemonDust.GetComponent<DaemonAura>().activateAura = false;
+		GetComponent<SpriteRenderer>().sprite = DaemonAura;
 		StopCoroutine("removeBuff");
 	}
 }
